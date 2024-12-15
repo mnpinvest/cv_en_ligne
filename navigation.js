8845d827cc2904d53f5d9492156b36416f7506be
Mnari <160485361+mnpinvest@users.noreply.github.com>
2024-12-15 17:24:25 +0100

    Create navigation.js

---
+++ navigation.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('nav a');
+    links.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            } else {
+                console.error('Section avec l\'ID ' + targetId + ' non trouvée.');
+            }
+        });
+    });
+});
