cb7a691e65e787203b8bcd162511ba5a72144d09
Mnari <160485361+mnpinvest@users.noreply.github.com>
2024-12-15 19:40:38 +0100

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const links = document.querySelectorAll('nav a');
+    const sections = document.querySelectorAll('.section');
+
     links.forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
             const targetId = this.getAttribute('href').substring(1);
             const targetElement = document.getElementById(targetId);
+
+            // Masquer toutes les sections
+            sections.forEach(section => {
+                section.querySelector('.section-content').classList.remove('open');
+            });
+
+            // Afficher la section cible
             if (targetElement) {
+                targetElement.querySelector('.section-content').classList.add('open');
                 targetElement.scrollIntoView({ behavior: 'smooth' });
-            } else {
-                console.error('Section avec l\'ID ' + targetId + ' non trouvée.');
             }
         });
     });
