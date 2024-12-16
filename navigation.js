5546092dfd3e1bb61f91f6f080dbca962129e54c
Mnari <160485361+mnpinvest@users.noreply.github.com>
2024-12-16 09:29:12 +0100

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,20 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const links = document.querySelectorAll('nav a');
-    const sections = document.querySelectorAll('.section');
+    const sections = document.querySelectorAll('.section-content');
 
     links.forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
-            const targetId = this.getAttribute('href').substring(1); // Récupérer l'ID cible sans le #
-            const targetElement = document.getElementById(targetId); // Trouver l'élément correspondant
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId).querySelector('.section-content');
 
             if (targetElement) {
-                // Masquer toutes les sections
                 sections.forEach(section => {
                     section.classList.remove('open');
                 });
-
-                // Afficher la section cible
                 targetElement.classList.add('open');
                 targetElement.scrollIntoView({ behavior: 'smooth' });
             } else {
